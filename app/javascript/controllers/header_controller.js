b56a0a2057c58df189341c6629af388817f8a75d
conh-2027 <nguyen.huu.co@sun-asterisk.com>
2023-06-30 23:04:33 +0700

    feat(login): add gem devise, add view

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -1,20 +1,21 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ['people', 'cart']
+  static targets = ['people', 'cart', 'sign_in']
 
   connect() {
-    var women_list = document.getElementsByClassName('women-list');
-    var men_list = document.getElementsByClassName('men-list');
-    var shopping_cart = document.getElementsByClassName('shopping-cart');
+    let women_list = document.getElementsByClassName('women-list');
+    let men_list = document.getElementsByClassName('men-list');
+    let shopping_cart = document.getElementsByClassName('shopping-cart');
+    let form_login = document.getElementsByClassName('body-form-login');
 
     toggle(this.peopleTargets[0], women_list);
     toggle(this.peopleTargets[1], men_list);
     toggle(this.cartTarget, shopping_cart);
+    toggle(this.sign_inTarget, form_login);
 
     function toggle(target, element) {
       target.addEventListener("click", function(){
-
         if(element[0].classList.contains("hidden")){
           element[0].classList.remove("hidden")
         }else{
